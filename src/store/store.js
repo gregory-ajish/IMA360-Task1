97a9550a0a47cac78916a0b6644bc12b32bc3b1c
gregory-ajish <gregory.ajish@ima360.com>
2026-09-18 00:22:58 +0530

    feat:add mock api+reux integration for dashboard

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import weatherReducer from './reducers/weatherReducer';
+import dashboardReducer from './reducers/dashboardReducer';
 
 export const store = configureStore({
   reducer: {
     weather: weatherReducer,
+    dashboard: dashboardReducer,
   },
 });
 
